dc91118c8171becf86daf3e2faa60eb930fd7b7f
Adrian Nowowiejski <anowowiejski123@gmail.com>
2024-08-08 12:37:00 +0200

    FPS movement & player setup

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class CameraMovement : NetworkBehaviour
+{
+	public float SensX;
+	public float SensY;
+
+	public GameObject cam;
+
+	float xRotation;
+	float yRotation;
+
+	private void Start()
+	{
+		if (!IsOwner)
+		{
+			cam.SetActive(false);
+			return;
+		}
+
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+	}
+	private void Update()
+	{
+		if (!IsOwner) return;
+		float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * SensX;
+		float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * SensY;
+
+		yRotation += mouseX;
+		xRotation -= mouseY;
+
+		xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+		transform.rotation = Quaternion.Euler(xRotation, yRotation, transform.rotation.z);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : NetworkBehaviour
+{
+    private float Hmove, Vmove;
+    private Rigidbody rb;
+    private Vector3 dir;
+    public float speed;
+
+    private void Start()
+    {
+        if (!IsOwner) return;
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (!IsOwner) return;
+        rb.MovePosition(transform.position + (transform.forward * Input.GetAxis("Vertical") * speed) + (transform.right * Input.GetAxis("Horizontal") * speed));
+    }
+}
