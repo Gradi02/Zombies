196644eeac75fa79e5f843a44394f74091c22a06
Tobkubos <tobkub7@gmail.com>
2024-08-10 12:12:39 +0200

    shooting

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -12,8 +12,12 @@ public class CameraMovement : NetworkBehaviour
 
 	float xRotation;
 	float yRotation;
-
-	private void Start()
+	public ParticleSystem PS_blood;
+    public ParticleSystem PS_other;
+    public GameObject gun;
+	private float Cooldown = 0.2f;
+    private float nextFireTime = 0f;
+    private void Start()
 	{
 		if (!IsOwner)
 		{
@@ -37,5 +41,39 @@ public class CameraMovement : NetworkBehaviour
 
 		cam.transform.rotation = Quaternion.Euler(xRotation, yRotation, transform.rotation.z);
 		transform.rotation = Quaternion.Euler(0, yRotation, 0);
-	}
+
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * 100);
+
+        if (Input.GetMouseButtonDown(0))
+		{
+			if (Time.time >= nextFireTime)
+			{
+                nextFireTime = Time.time + Cooldown;
+
+				if (Physics.Raycast(ray, out hit))
+				{
+					if (hit.collider.CompareTag("enemy"))
+					{
+						// hit
+						Debug.Log("HIT");
+						hit.collider.gameObject.GetComponent<Enemy>().TakeDamage(25);
+
+						// hit particle
+						ParticleSystem ps = Instantiate(PS_blood, hit.point, Quaternion.LookRotation(hit.normal));
+						Destroy(ps.gameObject, 2);
+					}
+					else
+					{
+                        ParticleSystem ps = Instantiate(PS_other, hit.point, Quaternion.LookRotation(hit.normal));
+						ps.GetComponent<Renderer>().material = hit.collider.GetComponent<Renderer>().material;
+                        Destroy(ps.gameObject, 2);
+                    }
+				}
+			}
+        }
+    }
 }
