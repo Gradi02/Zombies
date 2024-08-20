02f59249ed58c2d3ccd7b6ef494b71b6f46661d0
Adrian Nowowiejski <anowowiejski123@gmail.com>
2024-08-20 12:42:28 +0200

    Interaction System

---
+++ Assets/Items/ItemManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour, IInteractable
+{
+    public void MakeInteraction()
+    {
+        Debug.Log("Podnies item");
+    }
+
+
+}
