cfaa488c9cd4496b55477b2f92e2d3a4f7c832d1
Adrian Nowowiejski <anowowiejski123@gmail.com>
2024-08-21 14:56:54 +0200

    muzzle flash for weapon

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
 using TMPro;
+using UnityEngine.VFX;
 
 public class PlayerShooting : NetworkBehaviour
 {
-    public ParticleSystem PS_blood;
-    public ParticleSystem PS_other;
-    public GameObject gun;
-    private float Cooldown = 0.2f;
-    private float nextFireTime = 0f;
+	public ParticleSystem PS_blood;
+	public ParticleSystem PS_other;
+	public GameObject gun;
+	private float Cooldown = 0.3f;
+	private float nextFireTime = 0f;
 	private float reloadingTime = 3f;
 	public Camera cam;
-	[SerializeField] private TextMeshProUGUI ammoText;	
+	[SerializeField] private TextMeshProUGUI ammoText;
+	[SerializeField] private VisualEffect shootParticle;
+	[SerializeField] private GameObject flashlight;
 
 	[Header("Gun Stats")]
 	private Animator animator;
@@ -24,26 +27,27 @@ public class PlayerShooting : NetworkBehaviour
 
 
 	private void Awake()
-    {
+	{
 		animator = gun.GetComponent<Animator>();
 	}
 
-    void Update()
-    {
+	void Update()
+	{
 		if (!IsOwner && cam != null) return;
 
 		Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
 		RaycastHit hit;
 
-        if (Ammo <= 0)
-        {
+		if (Ammo <= 0 && Time.time > nextFireTime - 0.1f)
+		{
 			ammoText.text = "reloading";
 			nextFireTime = Time.time + reloadingTime;
+			CancelInvoke();
 			animator.CrossFade(reload.name, 0.2f);
-            Ammo = 8;
-        }
+			Ammo = 8;
+		}
 
-        if (Time.time >= nextFireTime)
+		if (Time.time >= nextFireTime)
 		{
 			ammoText.text = "ammo: " + Ammo;
 			if (Input.GetMouseButtonDown(0))
@@ -52,7 +56,8 @@ public class PlayerShooting : NetworkBehaviour
 
 				Ammo--;
 				ammoText.text = "ammo: " + Ammo;
-				animator.CrossFade(shoot.name, 0.2f);
+				RequestShootParticleServerRpc();
+				Invoke(nameof(PlayShootAnimDelayed), 0.15f);
 				AlarmNearEnemies();
 
 				if (Physics.Raycast(ray, out hit, Mathf.Infinity, obstacleMask))
@@ -67,8 +72,8 @@ public class PlayerShooting : NetworkBehaviour
 						ParticleSystem ps = Instantiate(PS_blood, hit.point, Quaternion.LookRotation(hit.normal));
 						Destroy(ps.gameObject, 2);
 					}
-					else if(hit.collider.CompareTag("body"))
-                    {
+					else if (hit.collider.CompareTag("body"))
+					{
 						// hit
 						Debug.Log("HIT - body");
 						hit.collider.transform.root.GetComponent<IDamage>().TakeDamage(damage);
@@ -101,8 +106,8 @@ public class PlayerShooting : NetworkBehaviour
 					}
 					else
 					{
-						if(hit.collider.GetComponent<Renderer>() != null)
-                        {
+						if (hit.collider.GetComponent<Renderer>() != null)
+						{
 							ParticleSystem ps = Instantiate(PS_other, hit.point, Quaternion.LookRotation(hit.normal));
 							ps.GetComponent<Renderer>().material = hit.collider.GetComponent<Renderer>().material;
 							Destroy(ps.gameObject, 2);
@@ -113,9 +118,27 @@ public class PlayerShooting : NetworkBehaviour
 		}
 	}
 
+	private void PlayShootAnimDelayed()
+	{
+		animator.CrossFade(shoot.name, 0.2f);
+	}
+
+	[ServerRpc]
+	private void RequestShootParticleServerRpc()
+	{
+		DisplayShootParticleClientRpc();
+	}
+
+	[ClientRpc]
+	private void DisplayShootParticleClientRpc()
+    {
+		shootParticle.Play();
+		flashlight.SetActive(true);
+	}
+
 	public LayerMask enemyLayer;
 	private void AlarmNearEnemies()
-    {
+	{
 		Collider[] hitColliders = Physics.OverlapSphere(transform.position, 400, enemyLayer);
 
 		foreach (Collider hitCollider in hitColliders)
